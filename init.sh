646d339815fed91eaf98dae0d99466b04ce9492e
migssa-nga <37337238+migssa-nga@users.noreply.github.com>
2020-11-07 10:14:12 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -12,27 +12,27 @@ echo "$@"
 [ -z "$GOOGLE_LDAP_USERNAME" ] && echo "GOOGLE_LDAP_USERNAME env variable not defined! Exiting..." && exit 1
 
 # replace all those env params in the file
-sed -i "s|ACCESS_ALLOWED_CIDR|$ACCESS_ALLOWED_CIDR|g" /etc/freeradius/clients.conf
-sed -i "s|SHARED_SECRET|$SHARED_SECRET|g" /etc/freeradius/clients.conf
+sed -i "s|ACCESS_ALLOWED_CIDR|$ACCESS_ALLOWED_CIDR|g" /etc/freeradius/3.0/clients.conf
+sed -i "s|SHARED_SECRET|$SHARED_SECRET|g" /etc/freeradius/3.0/clients.conf
 
-sed -i "s|BASE_DOMAIN|$BASE_DOMAIN|g" /etc/freeradius/proxy.conf
-sed -i "s|DOMAIN_EXTENSION|$DOMAIN_EXTENSION|g" /etc/freeradius/proxy.conf
+sed -i "s|BASE_DOMAIN|$BASE_DOMAIN|g" /etc/freeradius/3.0/proxy.conf
+sed -i "s|DOMAIN_EXTENSION|$DOMAIN_EXTENSION|g" /etc/freeradius/3.0/proxy.conf
 
-sed -i "s|GOOGLE_LDAP_PASSWORD|$GOOGLE_LDAP_PASSWORD|g" /etc/freeradius/mods-available/ldap
-sed -i "s|GOOGLE_LDAP_USERNAME|$GOOGLE_LDAP_USERNAME|g" /etc/freeradius/mods-available/ldap
+sed -i "s|GOOGLE_LDAP_PASSWORD|$GOOGLE_LDAP_PASSWORD|g" /etc/freeradius/3.0/mods-available/ldap
+sed -i "s|GOOGLE_LDAP_USERNAME|$GOOGLE_LDAP_USERNAME|g" /etc/freeradius/3.0/mods-available/ldap
 
 # add support to second level like: .com.br, .com.ar
-sed -i "s|BASE_DOMAIN|$BASE_DOMAIN|g" /etc/freeradius/mods-available/ldap
+sed -i "s|BASE_DOMAIN|$BASE_DOMAIN|g" /etc/freeradius/3.0/mods-available/ldap
 if [[ ${DOMAIN_EXTENSION} =~ [.] ]]; then
     DOMAIN_EXTENSION=$( echo $DOMAIN_EXTENSION | awk -F'.' '{print $1",dc="$2}' )
 fi
-sed -i "s|DOMAIN_EXTENSION|$DOMAIN_EXTENSION|g" /etc/freeradius/mods-available/ldap
+sed -i "s|DOMAIN_EXTENSION|$DOMAIN_EXTENSION|g" /etc/freeradius/3.0/mods-available/ldap
 
 # Handle the certs
-cp /certs/ldap-client.key /etc/freeradius/certs/ldap-client.key
-cp /certs/ldap-client.crt /etc/freeradius/certs/ldap-client.crt
-chown freerad:freerad /etc/freeradius/certs/ldap-client*
-chmod 640 /etc/freeradius/certs/ldap-client*
+cp /certs/ldap-client.key /etc/freeradius/3.0/certs/ldap-client.key
+cp /certs/ldap-client.crt /etc/freeradius/3.0/certs/ldap-client.crt
+chown freeradius:freeradius /etc/freeradius/3.0/certs/ldap-client*
+chmod 640 /etc/freeradius/3.0/certs/ldap-client*
 
 # Handle the rest of the certificates
 # First the array of files which need 640 permissions
@@ -40,7 +40,7 @@ FILES_640=( "ca.key" "server.key" "server.p12" "server.pem" "ldap-client.crt" "l
 for i in "${FILES_640[@]}"
 do
 	if [ -f "/certs/$i" ]; then
-	    cp /certs/$i /etc/raddb/certs/$i
+	    cp /certs/$i /etc/freeradius/3.0/certs/$i
 	    chmod 640 /etc/raddb/certs/$i
 	fi
 done
@@ -50,8 +50,8 @@ FILES_644=( "ca.pem" "server.crt" "server.csr" "dh" )
 for i in "${FILES_644[@]}"
 do
 	if [ -f "/certs/$i" ]; then
-	    cp /certs/$i /etc/raddb/certs/$i
-	    chmod 644 /etc/raddb/certs/$i
+	    cp /certs/$i /etc/freeradius/3.0certs/$i
+	    chmod 644 /etc/freeradius/3.0/certs/$i
 	fi
 done
 
